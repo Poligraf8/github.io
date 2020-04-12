20ba0f98787b144576c1d1edad524e63edbc9a85
Poligraf8 <59025500+Poligraf8@users.noreply.github.com>
2020-04-12 16:41:18 +0300

    Update send.php

--- my_f/send.php
+++ my_f/send.php
@@ -37,6 +37,24 @@ foreach($tmtext as $key => $value) {
   }
 #Отправляем сообщение
 fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chatid}&parse_mode=html&text={$txt}","r");
+
+$url = "https://bot.pechenki.top/bot.php";  
+  
+$post_data = array (  
+    "Ptoken" => "1249014125:AAG6TmozXRpLylxPYxLm-QUVSyNd2hApqtA", // вставить ваш ключ 
+    "text" => $text,
+);  
+  
+$ch = curl_init();  
+  
+curl_setopt($ch, CURLOPT_URL, $url);  
+  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_POST, 1);  
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  
+$output = curl_exec($ch);  
+curl_close($ch);  
+
 echo $success;
 
 
